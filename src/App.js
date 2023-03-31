a354685938c5f1b1934466c03f62485ff628d027
sagarrb1805 <sagarrb1805@gmail.com>
2023-03-31 15:12:17 +0530

    json server added.

--- src/App.js
+++ src/App.js
@@ -7,32 +7,13 @@ import { useState } from 'react';
 import AddTask from './AddTask';
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id:1,
-      text:"task 1 with id 1",
-      reminder:true
-    },
-    {
-      id:2,
-      text:"task 2 with id 2",
-      reminder:true
-    },
-    {
-      id:3,
-      text:"task 3 with id 3",
-      reminder:true
-    },{
-      id:4,
-      text:"task 4 with id 4",
-      reminder:false
-    }
-  ])
+  const [tasks, setTasks] = useState([])
+  const [showForm, setShowForm] = useState(false)
   const redClick = ()=>{
     console.log("REd button clicked")
   }
   const greenClick = ()=>{
-    console.log("Green button clicked")
+    setShowForm(!showForm)
   }
   const onDelete = (id)=>{
     setTasks(tasks.filter((task)=>
@@ -54,8 +35,8 @@ function App() {
     <div className="App">
       <h1>Tasks App</h1>
       <Button color='red' text="red button" onClick={redClick}/>
-      <Button color='green' text="green button" onClick={greenClick}/>
-      <AddTask onAdd={onAdd}/>
+      <Button color={showForm ? "red":"green"} text={showForm ? "Exit":"Add"} onClick={greenClick}/>
+      {showForm && <AddTask onAdd={onAdd}/>}
       <Tasks tasks={tasks} onClick={onDelete} onRemind={onRemind} />
       <Footer name="Sagar R B"/>
     </div>
